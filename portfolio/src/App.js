dc9b7c321e4eb42dabf31e9ee82844c0be6600e4
ndrbn0 <andrea.bruni1992@gmail.com>
2024-11-07 15:49:41 +0100

    fix

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,9 @@
-import { Contact } from "./components/Contact/Contact";
-import { Intro } from "./components/About/Intro";
-import { Navbar } from "./components/NavBar/Navbar";
-import { Skills } from "./components/Skills/Skills";
-import { Works } from "./components/Works/Works";
-import { Footer } from "./components/Footer/Footer";
+import { Contact } from "./components/Contact/Contact.js";
+import { Intro } from "./components/About/Intro.js";
+import { Navbar } from "./components/NavBar/Navbar.js";
+import { Skills } from "./components/Skills/Skills.js";
+import { Works } from "./components/Works/Works.js";
+import { Footer } from "./components/Footer/Footer.js";
 
 function App() {
   return (
